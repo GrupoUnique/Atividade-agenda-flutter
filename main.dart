2c05ab5e89fdd2df26a650b560966ae7d9243f23
GrupoUnique <60199122+GrupoUnique@users.noreply.github.com>
2021-06-14 20:42:14 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+//import 'package:app_3/ui/contact_page.dart';
+
+import 'package:flutter/material.dart';
+import 'ui/HomePage.dart';
+
+//Caio Henrique
+//Luiz Augusto
+//Thalles Shinji
+//Tiago Oliveira
+//Yuri Cavalcanti
+
+// Mudança nas cores da HomePage e Contact Page
+// Mudança de alguns textos
+// Mundança de Algumas dimensões
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
